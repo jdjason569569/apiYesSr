efacbe12245975816afa9764ccd8686a58692160
jdjason569569 <jdjason569@gmail.com>
2023-04-02 13:52:44 -0500

    init project

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'root',
+    password: 'root',
+    database: 'my_db',
+    entities: ['dist/**/*.entity{.ts,.js}'],
+    synchronize: false,
+    retryDelay: 3000,
+    retryAttempts: 10
+  }),TasksModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
