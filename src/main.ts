f196e41bf307f00b6ca863c7a9bf403dbd66c3a7
jdjason569569 <jdjason569@gmail.com>
2023-04-17 22:30:28 -0500

    a

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
   });
-  await app.listen(3001);
+  await app.listen(process.env.PORT  || 3001);
 }
 bootstrap();
