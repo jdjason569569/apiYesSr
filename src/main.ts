7533a05819c00aa3c9ca3727c5053fae6b9e5fa6
jdjason569569 <jdjason569@gmail.com>
2023-04-17 23:25:20 -0500

    a

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-import * as express from 'express';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   dotenv.config({ path: '.env' });
-  const server = express();
-  const app = await NestFactory.create(AppModule,
-    new ExpressAdapter(server),);
-  
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
 
-  const corsOptions: CorsOptions = {
-    origin: '*',
-  };
-  app.enableCors(corsOptions);
-  
-
-
+    origin:"https://si-senor.netlify.app/",
+    
+    });
   await app.listen(process.env.PORT  || 3001);
 }
 bootstrap();
