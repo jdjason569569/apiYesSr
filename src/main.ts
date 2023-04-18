55ea12a35ecfaac33a8591ec941b5e06f9652b83
jdjason569569 <jdjason569@gmail.com>
2023-04-17 22:51:06 -0500

    a

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import * as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   dotenv.config({ path: '.env' });
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
+  const server = express();
+  const app = await NestFactory.create(AppModule,
+    new ExpressAdapter(server),);
+  
+
+  const corsOptions: CorsOptions = {
     origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
-  });
+  };
+  app.enableCors(corsOptions);
+  
+
+
   await app.listen(process.env.PORT  || 3001);
 }
 bootstrap();
