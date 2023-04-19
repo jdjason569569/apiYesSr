ff475c1add78cad2b1bc1ef595ea64fa57cf7dc1
jdjason569569 <jdjason569@gmail.com>
2023-04-18 21:09:15 -0500

    cors enabled

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,7 @@ async function bootstrap() {
   dotenv.config({ path: '.env' });
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-
-    origin:"https://si-senor.netlify.app",
-    
+    origin:"*",
     });
   await app.listen(process.env.PORT  || 3001);
 }
