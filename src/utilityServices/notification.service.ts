d6234a4e54179a9c8e1fc66e0d1d7a29cdd40183
DESKTOP-B52VILD\david <jeison@goldenrace.com>
2023-05-13 19:32:09 -0500

    feat: add days logic

--- src/utilityServices/notification.service.ts
+++ src/utilityServices/notification.service.ts
@@ -8,20 +8,29 @@ import { Cron } from '@nestjs/schedule';
 @Injectable()
 export class NotificationService {
 
-    constructor(@InjectRepository(Task) private taskRepo: Repository<Task>) {
-        
-      
-      }
+    constructor(@InjectRepository(Task) private taskRepo: Repository<Task>) {}
 
     @Cron('0 * * * * *') // Ejecutar cada minuto en punto
     handleCron() {
-    console.log('tarea ejecutada');
-    //this.findAll();
+    this.findAll();
   }
 
       public async findAll(){
-        const task = await this.taskRepo.find();
-        console.log(task);
+        const tasks = await this.taskRepo.find();
+        
+        
+        const month = new Date(); 
+        
+        const numberMonth = month.getMonth() + 1;
+          const deadline = new Date(month.getFullYear(), numberMonth, 1); //fecha limite cada mes
+          
+          tasks.forEach(task =>{
+            const days = deadline.getTime() - task.date_register.getTime();
+            const diasFaltantes = Math.ceil(days / (1000 * 60 * 60 * 24));
+            console.log('diasFaltantes', diasFaltantes);
+          })
+        
+        
         
      }
 
